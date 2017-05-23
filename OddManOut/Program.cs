ffe9a6196902f9005f993311ac50c36ea6422416
Erik <erik.ra@gmail.com>
2017-05-23 23:37:21 +0200

    Add project files.

---
+++ OddManOut/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace OddManOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] outputs = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.ReadLine();
+                int[] invitations = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt32);
+                var invitationsCounted = invitations.GroupBy(a => a).Select(a => new { Number = a.Key, Count = a.Count() });
+                outputs[i] = $"Case #{i + 1}: " + invitationsCounted.OrderBy(a => a.Count).First().Number.ToString();
+
+            }
+            foreach (string s in outputs)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
